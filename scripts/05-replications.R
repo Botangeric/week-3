884c4c93229742f2f42ac2032ec9471aa9171873
Botangeric <113480167+Botangeric@users.noreply.github.com>
2024-09-19 19:00:36 -0400

    Add files via upload

---
+++ scripts/05-replications.R
@@ -0,0 +1,19 @@
+#### Preamble ####
+# Purpose: Replicated graphs from... [...UPDATE THIS...]
+# Author: bo tang
+# Date: 19 September 2024
+# Contact: qinghe.tang@mail.utoronto.ca
+# License: None
+# Pre-requisites: None
+# Any other information needed? None
+
+
+#### Workspace setup ####
+library(tidyverse)
+# [...UPDATE THIS...]
+
+#### Load data ####
+# [...ADD CODE HERE...]
+
+
+
